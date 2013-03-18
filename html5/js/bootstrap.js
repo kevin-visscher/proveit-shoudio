b4cec914c1f437d152d43ed4df05f6e3a2a5967c
dionbosschieter <dionbosschieter@074.013.092.145.hva.nl>
2013-03-18 15:20:13 +0100

    Wrong files deleted

--- html5/js/bootstrap.js
+++ html5/js/bootstrap.js
@@ -1,3 +1,10 @@
 $(window).load(function() {
 	$('#featured').orbit();
+	map = new OpenLayers.Map("demoMap");
+    map.addLayer(new OpenLayers.Layer.OSM());
+    map.zoomToMaxExtent();
 });
+
+function slideaway() {
+	$('#rechtermenu').fadeOut('slow');
+}
