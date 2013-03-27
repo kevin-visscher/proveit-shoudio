d1164aab9ee7eaccc2e99cc5e33ee0c8fb343c4a
Dion Bosschieter <dionbosschieter@gmail.com>
2013-03-27 12:39:53 +0100

    Started rebuilding the marker and popup code
    Starting new code for the popup and a click handler on a marker.

--- html5/js/json.js
+++ html5/js/json.js
@@ -21,13 +21,35 @@ function jsonLoaded(data) {
     
     /*// A popup with some information about our location 
     var popup = new OpenLayers.Popup.FramedCloud("Popup", 
-        myLocation.getBounds().getCenterLonLat(), null,
+        centerLocation.getBounds().getCenterLonLat(), null,
         '<a target="_blank" href="http://openlayers.org/">We</a> ' +
         'could be here.<br>Or elsewhere.', null,
         true // <-- true if we want a close (X) button, false otherwise
-    );*/
+    );
     // and add the popup to it.
-    //map.addPopup(popup);
+    map.addPopup(popup);*/
+    
+    var markers = null
+    var size = new OpenLayers.Size(36, 47);
+    var offset = new OpenLayers.Pixel(-(size.w / 2), -size.h);
+    var icon = new OpenLayers.Icon('http://www.waze.co.il/images/home.png', size, offset);
+    
+    var LL = new OpenLayers.LonLat(shoudioObjects[0].lon, shoudioObjects[0].lat).transform(fromProjection, toProjection);
+
+    if (markers == null) {
+        markers = new OpenLayers.Layer.Markers("Markers");
+        map.addLayer(markers);
+    }
+
+    var marker = new OpenLayers.Marker(LL, icon)
+    markers.addMarker(marker);
+    var popup = new OpenLayers.Popup.FramedCloud("test", LL, null,
+                "testksfdjslfkjsl",
+                anchor = null, true, null);
+    marker.events.register("click",this.marker,function(e){map.addPopup(popup);});
+    
+    //map.addPopup(   );
+    
     map.removeControl(map.getControl('OpenLayers.Control.Zoom_54'));
     map.removeControl(map.getControl('OpenLayers.Control.Attribution_56'));
 }
