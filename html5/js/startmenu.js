f1c8fbf91faaeed6e5463df93956d86bb4e2785a
Olaf Elzinga <olaffee@gmail.com>
2013-04-26 16:02:50 +0200

    Startmenu basic button toevoeging

--- html5/js/startmenu.js
+++ html5/js/startmenu.js
@@ -2,23 +2,28 @@
 function startmenu(item){
 	
 	//collection info ophalen
-	var address =item.address;
 	var naamrondje= item.name;	
-	var createdon= item.created_on;
 	var user_id = item.userid;
-	var description= item.description;
+	var address =item.address;
+
+	var createdon= item.created_on;
+	var rating_rate = item.rating_rate;
+	var rating_votes = item.rating_votes;
 	
 	//s3.amazonaws.com/noise.shoudio.com/avatars/{SIZING}/user_id.jpg
 	var sizing = "100x100";
 	var plaatje = '<img src="http://s3.amazonaws.com/noise.shoudio.com/avatars/'+sizing+'/'+ user_id+'.jpg" />';
 
+	var rating = rating_rate+ "/5";
 
 	// var zenden naar hTmL
- 	$('.startmenu_address').append(address);
  	$('.startmenu_naamrondje').append(naamrondje);
- 	$('.startmenu_createdon').append(createdon);
+ 	
  	$('.startmenu_plaatje').append(plaatje);
- 	$('.startmenu_description').append(description);
+ 	$('.startmenu_address').append(address);
+ 	
+ 	$('.startmenu_createdon').append(createdon);
+ 	$('.startmenu_rating').append(rating);
 
  	
 
